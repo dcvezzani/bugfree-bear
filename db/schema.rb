c64973d380e4412971c7d269a79dafe1631f526d
David Vezzani <david_vezzani@yahoo.com>
2013-09-06 14:49:20 -0700

    adds title column to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130905234247) do
+ActiveRecord::Schema.define(:version => 20130906214814) do
 
   create_table "issues", :force => true do |t|
     t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "title"
   end
 
   create_table "users", :force => true do |t|
